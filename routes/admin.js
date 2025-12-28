2b42f5c08001d6b5eb0516cc62dc84721979e500
Dipan <dipanmaulick25@gmail.com>
2025-12-28 20:22:14 +0000

    written user & admin signup,login routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,64 @@
 const express = require("express");
 const {Router} = require("express");
 const {AdminModel} = require("../db");
+const bcrypt = require("bcrypt");
 
 const adminRouter = Router();
 
-adminRouter.post("/signup" , function(req ,res){
-    res.json({
-        message : "admin acc created"
-    })
+adminRouter.post("/signup" , async function(req ,res){
+        const email = req.body.email;
+        const password = req.body.password;
+
+        let errorThrown = false;
+        try{
+            const hashedPassword = await bcrypt.hash(password, 5);
+
+            await AdminModel.create({
+                email : email,
+                password : hashedPassword
+            })
+        }catch(e){
+            res.json({
+                message : "error"
+            })
+            errorThrown = true;
+        }
+
+        if(!errorThrown){
+            res.json({
+                message : "admin account created successfully"
+            })
+        }
 })
 
-adminRouter.post("/login" , function(req ,res){
-    res.json({
-        message : "admin has logged in"
+adminRouter.post("/login" , async function(req ,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await AdminModel.findOne({
+        email : email
     })
+
+    if(!user){
+        res.json({
+            message : "no such admin exists"
+        })
+    }
+
+    const passwordMatch = bcrypt.compare(password , user.password);
+
+    if(passwordMatch){
+        const token = jwt.sign({
+            id : user._id.toString()
+        },process.env.JWT_SECRET)
+        res.json({
+            token : token
+        })
+    }else{
+        res.json({
+            message : "invalid credentials"
+        })
+    }
 })
 
 //admin creates course
--- routes/user.js
+++ routes/user.js
@@ -2,19 +2,71 @@ const express = require("express");
 //const Router = express.Router;
 const {Router} = require("express");
 const {UserModel} = require("../db");
+const bcrypt = require("bcrypt");
 
 const userRouter = Router();
 
-userRouter.post("/signup" , function(req ,res){
-        res.json({
-            message : "you have signed up"
+userRouter.post("/signup" , async function(req ,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let errorThrown = false;
+    try{
+        const hashedPassword = bcrypt.hash(password, 5);
+
+        await UserModel.create({
+            email : email,
+            password : hashedPassword
         })
+
+    }catch(e){
+        res.json({
+            message : "error"
+        })
+        errorThrown = true;
+
+    }
+
+    if(!errorThrown){
+        res.json({
+            message : "account created"
+        })
+    }
+        
 })
 
-userRouter.post("/login" , function(req ,res){
+userRouter.post("/login" , async function(req ,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let response = await UserModel.findOne({
+        email : email
+    })
+
+    if(!response){
         res.json({
-            message : "you have logged in"
+            message : "invalid email"
         })
+    }
+
+    const passwordMatch = bcrypt.compare(password,response.password);
+
+    if(passwordMatch){
+        const token = jwt.sign({
+                id : response._id.toString()
+        },process.env.JWT_SECRET);
+
+        return res.json({
+            token : token
+        })
+    }else{
+        res.json({
+            message : "Incorrect credentials"
+        })
+    }
+
+    
+        
 })
 
 userRouter.get("/purchases", function(req ,res){
